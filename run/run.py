ef902bd384d4f7936d34cefe8aebe37bea5ac33c
mrsata <liuhanrick@gmail.com>
2017-07-14 22:59:54 -0700

    Add keras

--- run/run.py
+++ run/run.py
@@ -1,33 +1,57 @@
 from __future__ import print_function
+
 import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
 from src.agent import Agent
 from src.env import Env
 
 
-def generate_baseline(n=40):
+def generate_baseline(nb_users):
     psi = np.array([0.065, 0.145, -0.495])
-    z1 = np.random.poisson(35, n)
-    z2 = np.random.binomial(1, 0.35, n)
-    z3 = np.random.normal(0, 5.29, n)
+    z1 = np.random.poisson(35, nb_users)
+    z2 = np.random.binomial(1, 0.35, nb_users)
+    z3 = np.random.normal(0, 5.29, nb_users)
     z = np.vstack((z1, z2, z3)).T
     b = z.dot(psi)
     return b
 
 np.random.seed(0)
-action_space = [0, 1]
-baseline = generate_baseline()
-env = Env(baseline, action_space)
-# print(env.reset())
-agent = Agent(action_space)
-wst = agent.warmup(env, nb_steps=50)
-# print(wst.shape)
+nb_users = 40
+state_size = 3
+learning_rate = 1e-2
+baseline = generate_baseline(nb_users=nb_users)
+env = Env(baseline=baseline, p=state_size)
+nb_actions = env.action_space.shape[-1]
 
-def print_transit(transits):
-    for i in range(len(transits)):
-        print("user #{}: ".format(i))
-        for j in range(len(transits[i])):
-            print(', '.join(map(str, transits[i, j])))
-        print("*" * 79)
+model = Sequential()
+model.add(Dense(10, input_dim=state_size, activation='relu'))
+model.add(Dense(6, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(nb_actions, activation='linear'))
+model.compile(loss='mse', optimizer=Adam(lr=learning_rate))
+print(model.summary())
 
-history = agent.fit(env, nb_steps=100, wst=wst)
-print_transit(history)
+agent = Agent(model=model, nb_users=nb_users, nb_actions=nb_actions,
+              state_size=state_size, gamma=.99)
+agent.warmup(env, nb_steps=50)
+history = agent.fit(env, nb_steps=100)
+
+agent2 = Agent(model=model, nb_users=nb_users, nb_actions=nb_actions,
+              state_size=state_size, gamma=.99)
+agent2.warmup(env, nb_steps=50)
+history2 = agent.fit(env, nb_steps=200)
+
+print(np.average(history.reshape(-1, history.shape[-1]), axis=0)[4])
+print(np.average(history2.reshape(-1, history2.shape[-1]), axis=0)[4])
+print(np.average(history.reshape(-1, history.shape[-1]), axis=0).shape)
+
+with open('data/history', 'w') as f:
+    for i in range(len(history)):
+        f.write("user #{}: \n".format(i))
+        for j in range(len(history[i])):
+            f.write(', '.join(map(str, history[i, j, :5])) + '\n')
+        f.write("*" * 79 + '\n')
--- src/agent.py
+++ src/agent.py
@@ -8,49 +8,73 @@ class Agent(object):
     a learned policy. Default policy of an agent is the random policy.
     """
 
-    def __init__(self, action_space=None):
-        self.action_space = action_space
+    def __init__(self, model, nb_users, nb_actions, state_size, gamma=.99):
+        self.model = model
+        self.nb_users = nb_users
+        self.nb_actions = nb_actions
+        self.state_size = state_size
+        self.gamma = gamma
+        self.memory = None
 
     def forward(self, state):
-        action = np.zeros(state.shape[0], )
-        ### TODO: implement this ###
-        
+        q_values = []
+        for s in state:
+            q_values.append(self.model.predict(s[np.newaxis, :]))
+        q_values = np.array(q_values).reshape(self.nb_users, self.nb_actions)
+        action = np.argmax(q_values, axis=1)
         return action
+
+    def backward(self, batch_size):
+        replay = self.memory.reshape(-1, self.memory.shape[-1])
+        mask = np.random.randint(replay.shape[0], size=batch_size)
+        minibatch = replay[mask, :]
+        for batch in minibatch:
+            state = batch[:self.state_size]
+            action = int(batch[self.state_size])
+            reward = batch[self.state_size + 1]
+            s_new = batch[-self.state_size:]
+            target = reward + self.gamma * \
+                np.amax(self.model.predict(s_new[np.newaxis, :])[0])
+            target_f = self.model.predict(state[np.newaxis, :])
+            target_f[0][action] = target
+            self.model.fit(state[np.newaxis, :], target_f, epochs=1, verbose=0)
 
     def warmup(self, env, nb_steps=50):
         state = env.reset()
-        action = np.random.choice(self.action_space, state.shape[0])
+        action = np.random.randint(self.nb_actions, size=state.shape[0])
         s_new, reward = env.step(state, action)
-        history = np.hstack((state,
-                             action[:, np.newaxis],
-                             reward[:, np.newaxis]))[:, np.newaxis, :]
-        action = np.random.choice(self.action_space, state.shape[0])
+        self.memory = np.hstack((state,
+                                action[:, np.newaxis],
+                                reward[:, np.newaxis],
+                                s_new))[:, np.newaxis, :]
+        action = np.random.randint(self.nb_actions, size=state.shape[0])
         state = s_new
         step = 1
         while step < nb_steps:
             s_new, reward = env.step(state, action)
             transit = np.hstack((state,
                                  action[:, np.newaxis],
-                                 reward[:, np.newaxis]))[:, np.newaxis, :]
-            history = np.concatenate((history, transit), axis=1)
-            action = np.random.choice(self.action_space, state.shape[0])
+                                 reward[:, np.newaxis],
+                                 s_new))[:, np.newaxis, :]
+            self.memory = np.concatenate((self.memory, transit), axis=1)
+            action = np.random.randint(self.nb_actions, size=state.shape[0])
             state = s_new
             step += 1
-        return history
+        return self.memory
 
-    def fit(self, env, nb_steps=100, wst=None):
-        state, action, reward, history, step = None, None, None, None, 0
+    def fit(self, env, nb_steps=100):
+        state, action, reward, step = None, None, None, 0
         state = env.reset()
         action = self.forward(state)
         s_new, reward = env.step(state, action)
         transit = np.hstack((state,
                              action[:, np.newaxis],
-                             reward[:, np.newaxis]))[:, np.newaxis, :]
-        if wst is not None:
-            history = wst
-            history = np.concatenate((history, transit), axis=1)
+                             reward[:, np.newaxis],
+                             s_new))[:, np.newaxis, :]
+        if self.memory is None:
+            self.memory = transit
         else:
-            history = transit
+            self.memory = np.concatenate((self.memory, transit), axis=1)
         action = self.forward(s_new)
         state = s_new
         step = 1
@@ -59,11 +83,14 @@ class Agent(object):
                 s_new, reward = env.step(state, action)
                 transit = np.hstack((state,
                                      action[:, np.newaxis],
-                                     reward[:, np.newaxis]))[:, np.newaxis, :]
-                history = np.concatenate((history, transit), axis=1)
+                                     reward[:, np.newaxis],
+                                     s_new))[:, np.newaxis, :]
+                self.memory = np.concatenate((self.memory, transit), axis=1)
                 action = self.forward(s_new)
                 state = s_new
                 step += 1
+                self.backward(32)
+                print step
         except KeyboardInterrupt:
             pass
-        return history
+        return self.memory
