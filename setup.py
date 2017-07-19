729c702bdcd82bba2ae84361fb06a7d4582c3c01
mrsata <liuhanrick@gmail.com>
2017-07-19 15:19:29 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='mobileHealth',
+      version='0.1.0',
+      description='Deep Reinforcement Learning for Mobile Health',
+      author='Han Liu',
+      author_email='liuhanrick@gmail.com',
+      url='https://github.com/mrsata/mobileHealth',
+      download_url='https://github.com/mrsata/mobileHealth/archive/master.zip',
+      license='MIT',
+      install_requires=['numpy', 'keras', 'tensorflow'],
+      packages=find_packages())
